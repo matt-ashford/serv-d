366d6bf2df3071d8ad8fbd476fd7570ba0c30cf7
Ashford <Matthew.Ashford@prc.gov>
2024-05-16 11:42:30 -0400

    added all routes:

---
+++ serv-d/src/MailClassPage/MailClassPage.jsx
@@ -0,0 +1,9 @@
+export const MailClassPage = (props) => {
+  const { mailClassName } = props;
+
+  return (
+    <>
+      <h2>you are rendring {mailClassName} </h2>
+    </>
+  );
+};
--- serv-d/src/main.jsx
+++ serv-d/src/main.jsx
@@ -6,6 +6,7 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import NotFoundPage from "./mailClassComponents/NotFound/NotFound.jsx";
 import AllMarketDominant from "./mailClassComponents/allMarketDominant/AllMarketDominant.jsx";
+import { MailClassPage } from "./MailClassPage/MailClassPage.jsx";
 
 const router = createBrowserRouter([
   {
@@ -18,7 +19,31 @@ const router = createBrowserRouter([
     element: <AllMarketDominant />,
     errorElement: <NotFoundPage />,
   },
-  // { path: "/profile", element: <Profile />, errorElement: <NotFoundPage /> },
+  {
+    path: "/first-class",
+    element: <MailClassPage mailClassName="First Class Mail" />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/marketing-mail",
+    element: <MailClassPage mailClassName="Marketing Mail" />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/periodicals",
+    element: <MailClassPage mailClassName="Periodicals" />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/package-services",
+    element: <MailClassPage mailClassName="Package Services" />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/special-services",
+    element: <MailClassPage mailClassName="Special Services" />,
+    errorElement: <NotFoundPage />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
